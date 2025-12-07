7f122b86df31e4deaa0210cd594ad7c03fd0affc
Tejashri Tagad <tagadteju45@gmail.com>
2025-12-07 18:51:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ const picture = [
   },
   {
     src: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1353&q=80",
-    text: "City on Winter"
+    text: "Sunny Beach"
   },
   {
     src: "https://images.unsplash.com/photo-1551009175-8a68da93d5f9?auto=format&fit=crop&w=1351&q=80",
-    text: "Mountain Cloud"
+    text: " City on Winter"
   },
   {
     src: "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?auto=format&fit=crop&w=1350&q=80",
-    text: "Sunny Beach"
+    text: "Mountain Cloud"
   }
 ];
 
